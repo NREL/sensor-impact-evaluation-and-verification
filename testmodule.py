1be3e4dd524e323ccbc2dba5d351c41a415eb68f
lz356 <33605314+lz356@users.noreply.github.com>
2022-06-30 12:22:50 -0400

    Update testmodule.py

--- testmodule.py
+++ testmodule.py
@@ -13,21 +13,21 @@ example_object.create_folder_structure()
 # Download simulation data from server
 example_object.download_data()
 
-# # Run feature selection analysis module
-# example_object.sensor_selection_analysis(feature_extraction_config=[['mean', 'std'], 4*12],
-#                                          feature_selection_config={'filter': [False, 0.0], # ['pearson', 0.5]
-#                                                                    'wrapper': False, #'wrapper': 'forward'
-#                                                                    'embedded': True},
-#                                          by_fault_type=False,
-#                                          top_n_features=10,
-#                                          rerun=True)
+# Run feature selection analysis module
+example_object.sensor_selection_analysis(feature_extraction_config=[['mean', 'std'], 4*12],
+                                         feature_selection_config={'filter': [False, 0.0], # ['pearson', 0.5]
+                                                                   'wrapper': False, #'wrapper': 'forward'
+                                                                   'embedded': True},
+                                         by_fault_type=False,
+                                         top_n_features=10,
+                                         rerun=True)
 
-# # Run sensor inaccuracy module
-# example_object.sensor_inaccuracy_analysis(Monte_Carlo_runs=12, rerun=True)
+# Run sensor inaccuracy module
+example_object.sensor_inaccuracy_analysis(Monte_Carlo_runs=12, rerun=True)
 
-# # Run sensor cost analysis module
-# example_object.sensor_cost_analysis(analysis_mode='single',
-#                                     baseline_sensor_set='default',
-#                                     candidate_sensor_set='default',
-#                                     objective_function_coefficients=[0.11, 150, 15643],
-#                                     rerun=True)
+# Run sensor cost analysis module
+example_object.sensor_cost_analysis(analysis_mode='single',
+                                    baseline_sensor_set='default',
+                                    candidate_sensor_set='default',
+                                    objective_function_coefficients=[0.11, 150, 15643],
+                                    rerun=True)
